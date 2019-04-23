8ad655d936e2b7315c34a040ef836c3b39a7bfea
Jaryl Sim <jaryl.sim@me.com>
2019-04-23 23:34:16 +0800

    Implement Hello World endpoint just to kick the tyres.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const debug = require('debug')('app');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(port, () => {
   debug(`listening on port ${port}`);
 });
