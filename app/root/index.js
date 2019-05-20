fbbba211c7b0e521234eebd87dd3acb448ead762
Jaryl Sim <jaryl.sim@me.com>
2019-05-21 00:09:25 +0800

    Switch to cookie-based session, that will persist accross restarts.

--- app/root/index.js
+++ app/root/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const path = require('path');
 const passport = require('passport');
-const session = require('express-session');
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 // const debug = require('debug')('app:root:index');
 
 const app = express();
 const passportConfig = require('./config/passport');
-const sessionConfig = require('./config/session')(app);
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET],
+  maxAge: 24 * 60 * 60 * 1000, // 24 hours
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(session(sessionConfig));
 
 app.use(passport.initialize());
 app.use(passport.session());
