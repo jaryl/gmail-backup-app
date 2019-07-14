5486e698576f27f1a59353bcd4ec364da400ca0c
Jaryl Sim <jaryl.sim@me.com>
2019-07-15 01:17:31 +0800

    Setup retry for GraphQL queries.

--- app/frontend/config/apolloConnect.js
+++ app/frontend/config/apolloConnect.js
@@ -1,12 +1,17 @@
 import ApolloClient from 'apollo-client';
 
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
+import { RetryLink } from 'apollo-link-retry';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
 
 const apolloConnect = (getToken) => {
-  const link = createHttpLink({ uri: 'http://localhost:4000/api' });
+  const link = ApolloLink.from([
+    new RetryLink(),
+    new HttpLink({ uri: 'http://localhost:4000/api' }),
+  ]);
 
   const withError = onError((error) => {
     console.error(error);
