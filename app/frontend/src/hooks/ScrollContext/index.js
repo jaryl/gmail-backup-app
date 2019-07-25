a9852d408df8bf82c45bb1165a2affcc4a78352c
Jaryl Sim <jaryl.sim@me.com>
2019-07-25 18:45:30 +0800

    Debounce scroll handler.

--- app/frontend/src/hooks/ScrollContext/index.js
+++ app/frontend/src/hooks/ScrollContext/index.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { debounce } from 'lodash';
 
 const ScrollContext = React.createContext();
 
 const ScrollContextProvider = (props) => {
   const [scrollPosition, setScrollPosition] = useState(0);
 
+  const debouncedCall = debounce(e => setScrollPosition(e.target.scrollTop), 100);
+
   const scrollHandler = (e) => {
-    setScrollPosition(e.target.scrollTop);
-    // TODO: debounce scroll handler
+    e.persist();
+    debouncedCall(e);
   };
 
   const values = {
